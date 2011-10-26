e30444dbe8155a0412a25ca700e398ca1d6d0208
Mixer <mixer@genopole.cnrs.fr>
2011-10-26 18:01:10 +0000

    version 1.0-1

--- R/init.R
+++ R/init.R
@@ -1,15 +1,15 @@
 .onLoad   <- function(lib, pkg) {
-  require(mixer, quietly=TRUE)
+  # require(mixer, quietly=TRUE)
 }
 .onAttach <- function(libname, pkgname){
-  cat(""                                                                      ,
-      "----------------------------------------------------------------------",
-      ""                                                                      ,
-      "      'simone' package version 1.0-0"                                  ,
-      "      SIMoNe page (http://stat.genopole.cnrs.fr/software/simone)"      ,
-      ""                                                                      ,
-      "----------------------------------------------------------------------",
-      "Note that versions >= 1.0-0 are not compatible with versions < 1.0.0. ",
-      "----------------------------------------------------------------------",
-      sep = "\n")
+    packageStartupMessage("")
+    packageStartupMessage("----------------------------------------------------------------------")
+    packageStartupMessage("")
+    packageStartupMessage("      'simone' package version 1.0-0")
+    packageStartupMessage("      SIMoNe page (http://stat.genopole.cnrs.fr/software/simone)")
+    packageStartupMessage("")
+    packageStartupMessage("----------------------------------------------------------------------")
+    packageStartupMessage("Note that versions >= 1.0-0 are not compatible with versions < 1.0.0. ")
+    packageStartupMessage("----------------------------------------------------------------------")
+
 }
